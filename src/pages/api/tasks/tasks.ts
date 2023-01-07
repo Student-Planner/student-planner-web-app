7dfea24d321fd180168fad6c0dc5415aa1feb3ef
SauceX <mahmoudzak4@gmail.com>
2023-01-07 15:14:50 +0300

    cleaned some packages

--- src/pages/api/tasks/tasks.ts
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import sampleEvents from "@/data/sampleEvents";
-import { Event } from "@prisma/client";
-
-export default function handler(req, res) {
-	const events: Event[] = sampleEvents;
-	res.status(200).json(events);
-}
