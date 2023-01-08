40d03cc87abaf192a68a7242fc5b83c47b1a72f9
SauceX <mahmoudzak4@gmail.com>
2023-01-08 16:54:52 +0300

    copy date header for events area

--- src/components/calendar/EventsArea.tsx
+++ src/components/calendar/EventsArea.tsx
@@ -19,9 +19,24 @@ function EventsArea({ }: Props) {
         setSelectedDayEvents(getDayEvents(selectedDayValue));
     }, [selectedDayValue]);
 
+    const copyText = async (e) => {
+        const anchor = e.target as HTMLButtonElement
+        const range = document.createRange()
+        range.selectNode(anchor)
+        window.getSelection()?.addRange(range)
+
+        try {
+            await navigator.clipboard.writeText(anchor.textContent)
+            console.log('Link copied to clipboard')
+        } catch (err) {
+            console.error('Unable to copy link: ', err)
+        }
+    }
+
+
     return (
-        <div className="flex flex-col container max-w-5xl mx-8 outline outline-2 px-4 py-4 rounded-lg outline-neutral-700 transition-colors duration-300 shadow-md hover:shadow-lg">
-            <h2 className="font-semibold text-lg px-2 text-gray-400"><span className="select-none">Schedule for </span><time dateTime={format(selectedDayValue, "yyyy-MM-dd")}>{format(selectedDayValue, "MMM dd, yyy")}</time>
+        <div className="flex flex-col container max-w-5xl mx-8 outline outline-2 px-4 py-4 rounded-lg select-none outline-neutral-700 transition-colors duration-300 shadow-md hover:shadow-lg">
+            <h2 className="font-semibold text-lg px-2 text-gray-400"><span>Schedule for</span><time dateTime={format(selectedDayValue, "yyyy-MM-dd")}><button className="bg-transparent hover:bg-neutral-800 hover:shadow-lg rounded-lg px-1.5 py-0.5" onClick={copyText}>{format(selectedDayValue, "MMM dd, yyy")}</button></time>
             </h2>
             {/* Events Portion */}
             <section className="">
